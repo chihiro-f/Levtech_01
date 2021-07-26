ee5ab65841f9fb1c3751940fa0ab2af58b76bfda
chihiro-f <cfnacchi@gmail.com>
2021-07-26 15:05:25 +0900

    第8章-3 課題2

---
+++ blog/resources/views/show.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            <h2 class="content__post">{{ $post->title }}</h2>
+                <p class="body">{{ $post->body }}</p>
+                <p class="updated_at">{{ $post->updated_at }}</p>
+        </div>
+        <div class="footer"><a href="/">戻る</a></div>
+    </body>
+</html>
